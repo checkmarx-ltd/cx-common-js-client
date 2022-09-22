ea2c973e02f81f26e821808cf83310fce66d4a29
jyotibhalerao <103122635+jyotibhalerao@users.noreply.github.com>
2022-09-22 18:27:43 +0530

    Added changes for SCA Resolver log messages and exploitable path in SCA resolver additional parameters. (#86)
    * Fixed bug proxyConfig is TRUE log message coming if proxy is disabled
    
    Fixed bug proxyConfig is TRUE log message coming if proxy is disabled.
    
    * Added log for SCA scan when SCA Resolver is enabled while executing command. (#85)
    
    * Added logs for SCA scan with SCA resolver while executing command.
    
    Added logs for SCA scan when SCA resolver is enabled while executing command.
    
    * Code changes for long switch parameters in SCA resolver additional parameters and removed result data on command execution part from log.
    
    Code changes for long switch parameters in SCA resolver additional parameters and removed result data on command execution part from SCA resolver logs.
    
    * Removed unwanted SCA resolver logs.
    
    Removed unwanted SCA resolver logs.
    
    * Code changes for exploitable path in SCA resolver for --sast-result-path specified
    
    Code changes for exploitable path in SCA resolver for --sast-result-path bundling  or zipping for absolute and directory path.
    
    * Code changes for Sca resolver additional parameters exploitable path
    
    Code changes for Sca resolver additional parameters exploitable path result file under timestamp based directory for -r and --sast-result-path for absolute and directory path
    
    * Code changes for -r absolute path in SCA Resolver and logs correction.
    
    Code changes for -r absolute path in SCA Resolver and logs correction for SCA Resolver.

--- src/services/clients/SpawnScaResolver.ts
+++ src/services/clients/SpawnScaResolver.ts
@@ -1,39 +1,31 @@
 import * as path from "path";
 import {spawn } from "child_process";
-
+import { Logger } from '../logger';
 
 export class SpawnScaResolver {
     
     private static readonly SCA_RESOLVER_EXE= "ScaResolver";
-    private static readonly OFFLINE = "offline";
-
+    private static readonly OFFLINE = "offline";    
+    
     /**
 	 * This method executes
 	 * @param pathToScaResolver - Path to SCA Resolver executable
 	 * @param scaResolverAddParams - Additional parameters for SCA resolver
 	 * @return
 	 */
-     static async runScaResolver(pathToScaResolver:string, scaResolverAddParams: string,pathToResultJSONFile:string ):Promise<number> {
+     static async runScaResolver(pathToScaResolver:string, scaResolverAddParams: string,pathToResultJSONFile:string, log: Logger):Promise<number> {
       let exitCode:number = -100;
       let scaResolverCommand: string;
-      let argument: Array<string>;
+      let argument: Array<string>;  
 
       //   Convert path and additional parameters into a single CMD command
       argument = scaResolverAddParams.split(" ");
       scaResolverCommand = pathToScaResolver + path.sep + SpawnScaResolver.SCA_RESOLVER_EXE;
       scaResolverCommand = scaResolverCommand + " " + SpawnScaResolver.OFFLINE;
       for(let i=0; i<argument.length;i++){
-        let arg: string  = argument[i];
-        if(arg=="debug")
-        {
-            arg = "Debug";
-        }
-        if(arg=="error")
-        {
-            arg = "Error";
-        }
+        let arg: string  = argument[i];        
         scaResolverCommand = scaResolverCommand + " " + arg;
-        if(arg=="-r")
+        if(arg=="-r" || arg == "--resolver-result-path")
         {
             scaResolverCommand =scaResolverCommand + " " +pathToResultJSONFile;
             i=i+1;
@@ -44,20 +36,47 @@ export class SpawnScaResolver {
 
 
     try{
+        let errorOccured = '';
         const child = spawn(scaResolverCommand, [''], {shell: true});
+        log.debug("Performing SCA scan through SCA Resolver.");
         exitCode = await new Promise( (resolve, reject) => {
-            child.on('close', resolve);
-        });
-    
-        if( exitCode) {
-            throw new Error( `subprocess error exit ${exitCode}`);
-        }
+            child.stdout.on("data", (x: any) => {
+              var data = x.toString();
+              data = data.replace(/(\r\n|\n|\r)/gm, "");
+              if(data !='' || data !=' ' ) {
+                log.debug(data);   
+              }             
+              });
+            
+            child.stderr.on("data", (x: any) => {                                       
+                errorOccured = x.toString();  
+                errorOccured = errorOccured.replace(/(\r\n|\n|\r)/gm, "");              
+                if(errorOccured !='')  {
+                log.debug('Error occurred while running SCA Resolver:' + errorOccured); 
+                }                       
+              });
+
+              child.on('error', (err: any) => { 
+                log.debug(`Error occurred while running SCA Resolver:  ${err}`); 
+                throw new Error(`Error occurred while running SCA Resolver.` + err);
+             });
+
+              child.on("exit", (code: any) => {
+                log.debug(`Subprocess exit with ${code}.`); 
+                log.debug('Finished SCA scan through SCA Resolver.');              
+              }); 
+
+          child.on('close', resolve);          
+    });
+
+    if (errorOccured !='') {
+        throw  Error(`Error occurred while running SCA Resolver. ${errorOccured}`);
+    }       
         
     }catch(err){
         throw Error(`Error occurred while running SCA Resolver.`+err);
     }
     return exitCode;    
-}
-   
+}   
 
 }
