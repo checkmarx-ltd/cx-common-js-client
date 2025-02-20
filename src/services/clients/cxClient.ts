0e744d67c6e665ec19b8e44dfdbcf46ccada3ba9
RahulP <rahul.pidde@checkmarx.com>
2025-02-20 18:48:21 +0530

    [ADO] ADO SAST plugin always generates JSON file with no new SAST vulnerabilities (#123)

--- src/services/clients/cxClient.ts
+++ src/services/clients/cxClient.ts
@@ -629,9 +629,7 @@ export class CxClient {
         result.queryList = CxClient.toJsonQueries(result, doc.Query);
 
         //Find if there are any new vulnerabilities
-        if(this.sastConfig.failBuildForNewVulnerabilitiesEnabled)
-            CxClient.getNewVulnerabilityCounts(result, doc.Query);
-        
+        CxClient.getNewVulnerabilityCounts(result, doc.Query);  
         // TODO: PowerShell code also adds properties such as newHighCount, but they are not used in the UI.
     }
 
