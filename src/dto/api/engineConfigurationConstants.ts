3daf5a7f3c7b81c77ee8f580150664c85f43b605
RahulP <rahul.pidde@checkmarx.com>
2025-03-05 17:57:55 +0530

    Q1 2025 integration branch (#125)

---
+++ src/dto/api/engineConfigurationConstants.ts
@@ -0,0 +1,8 @@
+export const engineConfigurationConstants: Record<number, string> = {
+    0: "Project Default",
+    1: "Default Configuration",
+    2: "Japanese (Shift-JIS)",
+    3: "Korean",
+    5: "Multi-language Scan",
+    6: "Fast Scan",
+};
